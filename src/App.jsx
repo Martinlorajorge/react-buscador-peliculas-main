02db02e8b525beddb128d368e175090d93f268af
Martin Lora <martinlorajorge@gmail.com>
2023-08-17 22:50:48 -0300

    standard

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,17 @@ function App () {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    // const { query } = Object.fromEntries(new window.FormData(event.target)) 
+    // const { query } = Object.fromEntries(new window.FormData(event.target))
   }
 
-  const handleChange=(event)=>{
+  const handleChange = (event) => {
     const newQuery = event.target.value
-    if (newQuery.startsWith(' '))return //Sie empieza con ' ' no deja poner espacio al comienzo
-    setQuery (event.target.value)
-    
+    if (newQuery.startsWith(' ')) return // Sie empieza con ' ' no deja poner espacio al comienzo
+    setQuery(event.target.value)
   }
 
-  useEffect(()=>{
-    if (query==='') {
+  useEffect(() => {
+    if (query === '') {
       setError('No se puede buscar pelicula vacia')
       return
     }
@@ -37,25 +36,24 @@ function App () {
       return
     }
 
-    if(query.length <3){
+    if (query.length < 3) {
       setError('La busqueda debe tener al menos 3 caracteres')
       return
     }
 
     setError(null)
-  },[query])
-
+  }, [query])
 
   return (
     <div className='page'>
 
       <header>
-      <h1>Buscador de Películas</h1>
+        <h1>Buscador de Películas</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input style={{border:'1px solid transparent', borderColor: error ? 'red': 'transparent'}} onChange={handleChange} value={query} name='query' placeholder='Avengers, Star Wars, The Matrix...' />
+          <input style={{ border: '1px solid transparent', borderColor: error ? 'red' : 'transparent' }} onChange={handleChange} value={query} name='query' placeholder='Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Buscar</button>
         </form>
-        {error && <p style={{color:'red'}}>{error}</p>}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
